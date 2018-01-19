9a1708bbc30a0014ea6777f1fda7c447af9ea7c6
Brian Dukes <bdukes@engagesoftware.com>
2018-01-18 21:33:46 -0600

    Add descriptions to tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,36 @@ const imagemin = require("gulp-imagemin");
 const sourcemaps = require("gulp-sourcemaps");
 const rename = require("gulp-rename");
 
-const basePath = "./Website/Portals/_default/Skins/Xcillion";
-
-gulp.task(function js() {
+const basePath = "./Website/Portals/_default/Skins/Xcillion/";
+function js() {
   return gulp
-    .src([basePath + "/**/*.js", "!" + basePath + "/**/*.min.js"])
+    .src([basePath + "**/*.js", "!" + basePath + "**/*.min.js"])
     .pipe(sourcemaps.init())
     .pipe(babel({ presets: ["env"] }))
     .pipe(uglify())
     .pipe(rename({ suffix: ".min" }))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(basePath));
-});
+}
+js.description =
+  "Transpile with Babel, minify with Uglify, add .min filename suffix, write sourcemaps";
+gulp.task(js);
 
-gulp.task(function img() {
+function img() {
   return gulp
     .src([
-      basePath + "/**/*.jpg",
-      basePath + "/**/*.png",
-      basePath + "/**/*.svg",
-      basePath + "/**/*.gif"
+      basePath + "**/*.jpg",
+      basePath + "**/*.png",
+      basePath + "**/*.svg",
+      basePath + "**/*.gif"
     ])
     .pipe(imagemin())
     .pipe(gulp.dest(basePath));
-});
+}
+img.description = "Minify jpg, png, svg, and gif images";
+gulp.task(img);
 
-gulp.task("default", gulp.parallel("js", "img"));
+const defaultTask = gulp.parallel("js", "img");
+defaultTask.displayName = "default";
+defaultTask.description = "Process JS and image files";
+gulp.task(defaultTask);
